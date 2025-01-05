4807e7c47e896a586a8ae0be0a5e81a595372c0b
Vitalii Zymukha <unknownvzzv@gmail.com>
2025-01-05 10:39:13 +0200

    Add media viewport filer

--- src/main/scala/com/unknownnpc/media/extractor/SeleniumMediaInCenterExtractor.scala
+++ src/main/scala/com/unknownnpc/media/extractor/SeleniumMediaInCenterExtractor.scala
@@ -2,7 +2,7 @@ package com.unknownnpc.media.extractor
 
 import com.typesafe.scalalogging.StrictLogging
 import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
-import org.openqa.selenium.{By, Cookie, WebDriver, WebElement}
+import org.openqa.selenium.*
 
 import java.net.URL
 import scala.jdk.CollectionConverters.*
@@ -42,23 +42,41 @@ private[extractor] trait SeleniumMediaInCenterExtractor extends Extractor with S
         driver.manage().addCookie(seleniumCustomCookie)
       )
       driver.navigate().refresh()
-      Thread.sleep(60_000) // Otherwise, all these async images are not ready
+      Thread.sleep(15_000) // TODO: Refactor. Otherwise, all these async images are not ready
       driver.findElements(By.tagName(tagForSearch)).asScala.toList
 
     tryMedia match
       case Failure(exception) =>
         Left(Exception.ProcessingError(exception.toString))
-      case Success(media) =>
-        logger.info(s"Found the next media: \n${media.mkString("\n")}")
+      case Success(medias) =>
 
-        if media.isEmpty then
+        val visibleInViewport = medias.filter { element =>
+          val jsExecutor = driver.asInstanceOf[JavascriptExecutor]
+          val isInViewport = jsExecutor.executeScript(
+            """return (function(el) {
+                 const rect = el.getBoundingClientRect();
+                 return (
+                   rect.top >= 0 &&
+                   rect.left >= 0 &&
+                   rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                   rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+                 );
+              })(arguments[0]);""",
+            element
+          ).asInstanceOf[Boolean]
+          isInViewport
+        }
+
+        logger.info(s"Found in the viewport: \n${visibleInViewport.mkString("\n")}")
+
+        if visibleInViewport.isEmpty then
           logger.info(s"No media found for $tagForSearch")
           Right(None)
         else
           val screenCenterX = ScreenWidth / 2
           val screenCenterY = ScreenHeight / 2
 
-          val nearestMedia = media.minBy { img =>
+          val nearestMedia = visibleInViewport.minBy { img =>
             val rect = img.getRect
             val imgCenterX = rect.getX + rect.getWidth / 2
             val imgCenterY = rect.getY + rect.getHeight / 2
--- src/test/scala/com/unknownnpc/media/extractor/SeleniumImageInCenterExtractorTest.scala
+++ src/test/scala/com/unknownnpc/media/extractor/SeleniumImageInCenterExtractorTest.scala
@@ -31,3 +31,17 @@ class SeleniumImageInCenterExtractorTest extends AnyFunSuite with Matchers:
     val payload = result.getOrElse(throw new RuntimeException("Boom")).get
     assert(payload.url.toString == "https://store.onlyfans.com/cdn/shop/files/ONLYFANSOct2-Square-3057.jpg?v=170")
     assert(payload.extension == Extension.JPEG)
+
+  test("SeleniumVideoInCenterExtractor extracts image from twitter when credentials are set"):
+    val cookies = sys.env.get("TEST_CUSTOM_COOKIES").map(CustomCookie.from).getOrElse(Seq.empty)
+
+    if cookies.nonEmpty then
+      val extractor = new SeleniumImageInCenterExtractor(cookies)
+
+      val result = extractor.extract(new URL("https://x.com/alinkalovesyouu/status/1874864689865466036"))
+
+      val payload = result.getOrElse(throw new RuntimeException("Boom")).get
+      assert(payload.url.toString == "https://pbs.twimg.com/media/GgTbfhIW0AA9gwI?format=jpg&name=900x900")
+      assert(payload.extension == Extension.JPEG)
+    else
+      assert(true)
