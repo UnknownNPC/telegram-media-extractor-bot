ba798fceba9548ffadfa45d857f95011cc5708d8
Vitalii Zymukha <unknownvzzv@gmail.com>
2025-01-18 19:39:01 +0200

    Add integration test

---
+++ src/test/scala/com/unknownnpc/media/extractor/SeleniumMediaTypeRecognizerTest.scala
@@ -0,0 +1,23 @@
+
+package com.unknownnpc.media.extractor
+
+import com.unknownnpc.media.extractor.model.MediaType.U3M8Page
+import com.unknownnpc.media.extractor.model.{CustomCookie, MediaType}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import java.net.URL
+
+class SeleniumMediaTypeRecognizerTest extends AnyFunSuite with Matchers:
+
+  test("SeleniumMediaTypeRecognizer should recognize page with m3u8 streams"):
+    val cookies = sys.env.get("TWITTER_CUSTOM_COOKIES").map(CustomCookie.from).getOrElse(Seq.empty)
+
+    if cookies.nonEmpty then
+      val recognizer = new SeleniumMediaTypeRecognizer(cookies)
+
+      val result = recognizer.getMediaType(new URL("https://x.com/SweetieFox1/status/1880631773824008463"))
+
+      assert(result == U3M8Page)
+    else
+      assert(true)
