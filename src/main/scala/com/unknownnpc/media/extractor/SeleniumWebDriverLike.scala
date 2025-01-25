ce367e5de0c036b70c7f315a5cebebd4be08ff96
Vitalii Zymukha <unknownvzzv@gmail.com>
2025-01-25 23:24:30 +0200

    Increase XHR requests logs analyzer timeout

--- src/main/scala/com/unknownnpc/media/extractor/SeleniumWebDriverLike.scala
+++ src/main/scala/com/unknownnpc/media/extractor/SeleniumWebDriverLike.scala
@@ -82,7 +82,7 @@ trait SeleniumWebDriverLike extends StrictLogging:
               logger.info("All XHR requests completed.")
               break(true)
 
-            Thread.sleep(1000)
+            Thread.sleep(5000)
 
           logger.error("Timeout while waiting for XHR requests to complete.")
           false
